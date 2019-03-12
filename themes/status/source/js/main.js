79a0105d523a7ff80cd96d9170ccadb4a6cee9dc
Nistor Cristian <contact@nistorcristian.com>
2019-03-12 14:33:15 +0200

    Responsive Header, Footer, Homepage. Changed Apps to Get

--- themes/status/source/js/main.js
+++ themes/status/source/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function ($) {
+
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
     $('.gallery-image img').each(function(index, el) {
         var container = $(this).closest('.gallery-image');
         var width = $(this)[0].naturalWidth;
@@ -6,4 +10,37 @@ $(document).ready(function ($) {
         var ratio = width / height;
         container.attr('style', 'flex: ' + ratio + ' 1 0%');
     });
+
+    mobileMenu(w);
+    mobileFooterMenu(w);
+
+    $(window).on('resize', function(event) {
+        w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        mobileMenu(w);
+        mobileFooterMenu(w);
+    });
+
+    function mobileMenu(w){
+        if(w < 1199){
+            $('header nav, header .btns').appendTo('.mobile-nav');
+        }else{
+            $('.mobile-nav nav, .mobile-nav .btns').insertBefore('.mobile-nav-trigger');
+        }
+    }
+
+    function mobileFooterMenu(w){
+        if(w < 768){
+            $('footer .navigation h5 a').attr('aria-expanded', 'false').addClass('collapsed');
+            $('footer .collapse').removeClass('show');
+        }else{
+            $('footer .navigation h5 a').attr('aria-expanded', 'true').removeClass('collapsed');
+            $('footer .collapse').addClass('show');
+        }
+    }
+
+    $('.mobile-nav-trigger-close, .mobile-nav-trigger').on('click', function (event) {
+        event.preventDefault();
+        $('body').toggleClass('nav-active');
+    });
+
 });
